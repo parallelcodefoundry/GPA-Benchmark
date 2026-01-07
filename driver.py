58e0ae7960e3167aac187615569f513135e48da0
Josh Davis <jhdavis@umd.edu>
2026-01-07 00:09:47 -0500

    Fix bugs in driver

--- driver.py
+++ driver.py
@@ -12,7 +12,7 @@ import yaml
 
 NCU_ARGS = ["--metrics",
             "regex:sm__inst_executed_pipe_[^.]*.avg.pct_of_peak_sustained_active$,regex:sm__sass_thread_inst_executed_op.*sum$,regex:l1tex__t_set_.*_pipe_lsu_mem_global_op_ld.sum$,regex:l1tex__t_set_accesses.sum$,regex:l1tex__t_requests.sum$,regex:l1tex__m_xbar2l1tex_read_sectors.sum$,sm__average_thread_inst_executed_pred_on_per_inst_executed_realtime,regex:sm__sass_inst_executed.*sum$,regex:sm__inst_issued.avg.per_cycle_active$,regex:.*throughput.avg.pct_of_peak_sustained_active$,regex:.*throughput.avg.pct_of_peak_sustained_elapsed$]",
-            "--set", "full", "--import-source=yes"]
+            "--set", "full", "--import-source", "yes", "--target-processes", "all"]
 
 def swap_file_in_app(app: dict, swap_file_path: str) -> None:
     """Swap the file in the application directory on disk. Back up original file. If the file
@@ -49,22 +49,21 @@ def swap_file_out_app(app: dict) -> None:
     shutil.copy(backup_path, dest_path)
 
 
-def build_app(app: dict, sm_version: int, no_clean: bool, cuda_home: str) -> None:
+def build_app(app: dict, sm_version: int, no_clean: bool, env: dict) -> None:
     """Build the application."""
     build_path = app["path"] if "build_path" not in app else app["build_path"]
     if "rodinia" in build_path:
         build_path = "rodinia"
-    if not no_clean:
-        subprocess.run(app["clean_command"].split() if "clean_command" in app
-                       else ["make", "clean"],
-                       cwd=build_path, check=True)
+    if not no_clean and "clean_command" in app:
+        subprocess.run(app["clean_command"].split(), cwd=build_path, check=True)
     build_command = "make -j 8" if "build_command" not in app else app["build_command"]
     build_command += f" SM_VERSION={sm_version}"
-    env = {"CUDA_HOME": cuda_home}
-    subprocess.run(build_command.split(), cwd=build_path, env=env, check=True)
+    build_command = build_command.split()
+    print(" ".join(build_command))
+    subprocess.run(build_command, cwd=build_path, env=env, check=True)
 
 
-def run_app(app: dict, cuda_home: str) -> None:
+def run_app(app: dict, env: dict) -> None:
     """Run the application."""
     if "run_path" in app:
         run_path = app["run_path"]
@@ -72,85 +71,101 @@ def run_app(app: dict, cuda_home: str) -> None:
         run_path = app["build_path"]
     else:
         run_path = app["path"]
-    run_command = app["run_command"]
-    env = {"CUDA_HOME": cuda_home}
-    subprocess.run(run_command.split(), cwd=run_path, env=env, check=True)
+    run_command = app["run_command"].split()
+    print(" ".join(run_command))
+    subprocess.run(run_command, cwd=run_path, env=env, check=True)
 
 
-def nsys_profile_app(app: dict, cuda_home: str) -> None:
+def nsys_profile_app(app: dict, env: dict) -> None:
     """Profile the application with Nsight Systems."""
-    nsys_command = ["nsys", "profile", "-o", app["name"] + ".nsys", "-f", "true"]
+    profile_dir = setup_profile_dir()
+    nsys_command = ["nsys", "profile", "-o", os.path.join(profile_dir, app["name"]), "-f", "true"]
     nsys_command.extend(app["run_command"].split())
-    nsys_command.extend(NCU_ARGS)
-    env = {"CUDA_HOME": cuda_home}
     if "run_path" in app:
         run_path = app["run_path"]
     elif "build_path" in app:
         run_path = app["build_path"]
     else:
         run_path = app["path"]
+    print(" ".join(nsys_command))
     subprocess.run(nsys_command, cwd=run_path, env=env, check=True)
 
 
-def ncu_profile_app(app: dict, cuda_home: str) -> None:
+def ncu_profile_app(app: dict, env: dict) -> None:
     """Profile the application with Nsight Compute."""
-    ncu_command = ["ncu", "-o", app["name"] + ".ncu", "-f", "true"]
-    ncu_command.extend(app["ncu_args"].split())
+    profile_dir = setup_profile_dir()
+    ncu_command = ["ncu", "-o", os.path.join(profile_dir, app["name"]), "-f", "true"]
+    if "ncu_args" in app:
+        ncu_command.extend(app["ncu_args"].split())
+    ncu_command.extend(NCU_ARGS)
     ncu_command.extend(app["run_command"].split())
-    env = {"CUDA_HOME": cuda_home}
     if "run_path" in app:
         run_path = app["run_path"]
     elif "build_path" in app:
         run_path = app["build_path"]
     else:
         run_path = app["path"]
+    print(" ".join(ncu_command))
     subprocess.run(ncu_command, cwd=run_path, env=env, check=True)
+
+
+def setup_profile_dir() -> str:
+    """Setup the profile directory."""
+    profile_dir: str = os.path.join(os.path.dirname(os.path.abspath(__file__)), "profiles")
+    if not os.path.exists(profile_dir):
+        os.makedirs(profile_dir)
+    return profile_dir
 
 
 def main() -> None:
     """Main function for driver."""
     parser = argparse.ArgumentParser()
     parser.add_argument("--app", type=str, default="all", help="The application to run")
-    parser.add_argument("--sm_version", type=int, default=90, help="The SM version to use")
+    parser.add_argument("--sm-version", type=int, default=90, help="The SM version to use")
     parser.add_argument("--cuda-home", type=str, default=None,
                         help="Path to the CUDA installation to use")
     parser.add_argument("--no-clean", action="store_true",
                         help="Do not clean the application before building")
     parser.add_argument("--build", action="store_true", help="Only build the application")
-    parser.add_argument("--nsys_profile", action="store_true",
+    parser.add_argument("--nsys", action="store_true",
                         help="Profile the application with Nsight Systems")
-    parser.add_argument("--ncu_profile", action="store_true",
+    parser.add_argument("--ncu", action="store_true",
                         help="Profile the application with Nsight Compute")
-    parser.add_argument("--config_file", type=str, default="driver_apps.yaml",
+    parser.add_argument("--config", type=str, default="driver_apps.yaml",
                         help="The app config file to use")
-    parser.add_argument("--swap-file-path", type=str, default=None,
+    parser.add_argument("--swaps", type=str, default=None,
                         help="The path to the directory containing code files to swap in for the " \
                             + "kernel, with the filename being app_name.swap, app file path to " \
                             + "swap for is set in the config file")
     args = parser.parse_args()
-    app_config: list[dict] = yaml.load(open(args.config_file, "r", encoding="utf-8"),
-                                       Loader=yaml.FullLoader)
-    if args.swap_file_path:
-        swap_files = os.listdir(args.swap_file_path)
+    app_config: dict = yaml.load(open(args.config, "r", encoding="utf-8"),
+                                 Loader=yaml.FullLoader)
+    if args.app != "all" and args.app not in [app["name"] for app in app_config["apps"]]:
+        raise ValueError(f"Application {args.app} not found in config file {args.config}")
+    if args.swaps:
+        swap_files = os.listdir(args.swaps)
     else:
         swap_files = None
-    if args.cuda_home:
-        cuda_home = args.cuda_home
-    else:
-        cuda_home = os.getenv("CUDA_HOME")
-    for app in app_config:
+    cuda_home = (args.cuda_home or
+                 os.getenv("CUDA_HOME") or
+                 os.getenv("CUDA_PATH") or
+                 os.getenv("CUDA_ROOT") or
+                 "/usr/local/cuda")
+    env = os.environ.copy()
+    env["CUDA_HOME"] = cuda_home
+    for app in app_config["apps"]:
         if args.app != "all" and app["name"] != args.app:
             continue
         if swap_files and app["name"] in swap_files:
-            swap_file_in_app(app, args.swap_file_path)
-        build_app(app, args.sm_version, args.no_clean, cuda_home)
+            swap_file_in_app(app, args.swaps)
+        build_app(app, args.sm_version, args.no_clean, env)
         if args.build:
             continue
-        run_app(app, cuda_home)
-        if args.nsys_profile:
-            nsys_profile_app(app, cuda_home)
-        if args.ncu_profile:
-            ncu_profile_app(app, cuda_home)
+        run_app(app, env)
+        if args.nsys:
+            nsys_profile_app(app, env)
+        if args.ncu:
+            ncu_profile_app(app, env)
         if swap_files and app["name"] in swap_files:
             swap_file_out_app(app)
 
