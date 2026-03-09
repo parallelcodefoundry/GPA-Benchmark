33fa8a0d98b243997cb2e8778219b1f670acbf54
Josh Davis <jhdavis@umd.edu>
2026-03-09 17:16:37 -0400

    Fix incorrect lulesh name in racecheck skip

--- gpa_bench_driver/gpa_bench_driver.py
+++ gpa_bench_driver/gpa_bench_driver.py
@@ -255,7 +255,7 @@ def _run_sanitize_phase(
     result.sanitize_stderrs = dict.fromkeys(tools, "")
     result.sanitize_details = dict.fromkeys(tools, False)
     for i, tool in enumerate(tools):
-        if tool == SanitizeTool.RACECHECK and get_canonical_app_name(ctx.app["name"]) == "LULESH":
+        if tool == SanitizeTool.RACECHECK and get_canonical_app_name(ctx.app["name"]) == "lulesh":
             logger.debug("Skipping racecheck for LULESH")
             if ctx.pbar is not None:
                 ctx.pbar()
